e16d163153beedd06c52af210be01321a1fcda58
Shenru <hushenru0@gmail.com>
2018-07-22 14:24:39 -0700

    Initial commit

---
+++ Assets/ObstacleGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour {
+
+    public GameObject obstacle;
+    public GameObject Player;
+    public float timeBetween = 3.0f;
+    public float nextOblectDiatant = 30.0f;
+    public float nextObjectTime;
+
+    // Use this for initialization
+    private void Start()
+    {
+        nextObjectTime = Time.time;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Time.time > nextObjectTime)
+        {
+
+            Instantiate(obstacle, new Vector3(Player.transform.position.x, -4, Player.transform.position.z + nextOblectDiatant), Quaternion.identity);
+            nextObjectTime = Time.time + timeBetween;
+        }
+
+        //TODO1 Instantiate the object here
+        //Quaternion.Euler is the rotation of your object
+
+        //
+
+        //TODO2 Use an if statment to control when the object spawns
+
+        /* There are two main ways of spawning objects
+        *    
+        * One version is to spawn a new one every few seconds. 
+        * You can find the number of seconds since your game started by using Time.time
+        * 
+        * The other version is to spawn a new one after the player has travelled a certain distance.
+        * To do this you will need the z position of the player
+        */
+
+
+    }
+
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public Rigidbody playerRB;
+    public float speed;
+    public float jumpForce;
+    
+    // Use this for initialization
+	void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Move();
+        Jump();
+        //TODO4 add jumping behaviour
+	}
+
+    private void Move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        //TODO1 create variable for moveForward
+        float moveForward = 1;
+        //TODO2 change x,z to the correct variables 
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveForward); 
+
+        //TODO3 make public variable speed and set it to 5 in the inspector
+        playerRB.AddForce(movement * speed);
+    }
+
+    private bool CanJump()
+    {
+        
+        if((playerRB.position.y >= -3.5 && playerRB.position.y < -2) || (playerRB.position.y > -2 && playerRB.position.y < -2.00))
+        {
+            return true;
+        }
+        return false;
+        
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && CanJump())
+        {
+            playerRB.AddForce(0, jumpForce, 0);
+        }
+
+        /*
+         Hints: 
+                Figure out which key you want to use to jump. How does Unity find that key?
+                We want to make the player jump by adding a force to it. What direction should the force be?
+                Google is your friend, and feel free to ask your intructors questions!
+        */
+
+    }
+}
